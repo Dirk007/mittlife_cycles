7cec272e204889bffe586e785959a3575f4d965a
Fabian Weik <fabian.weik@protonmail.com>
2024-09-25 20:10:42 +0200

    implement actix headers

--- src/verification/actix_headers.rs
+++ src/verification/actix_headers.rs
@@ -1,1 +1,28 @@
+use actix_web::http::header::HeaderMap;
+use anyhow::anyhow;
 
+use super::MappedHeaders;
+
+impl TryFrom<&HeaderMap> for MappedHeaders {
+    type Error = anyhow::Error;
+
+    fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
+        let serial = extract_header(headers, "X-Marketplace-Signature-Serial")?;
+        let algorithm = extract_header(headers, "X-Marketplace-Signature-Serial")?;
+        let signature = extract_header(headers, "X-Marketplace-Signature-Serial")?;
+
+        Ok(MappedHeaders {
+            serial,
+            algorithm,
+            signature,
+        })
+    }
+}
+
+fn extract_header(headers: &HeaderMap, key: &str) -> anyhow::Result<String> {
+    Ok(headers
+        .get(key)
+        .ok_or(anyhow!("Missing {} header", key))?
+        .to_str()?
+        .to_string())
+}
