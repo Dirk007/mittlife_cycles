ba306da6497d3d309d65e7335f8a5a68f9ef1877
Fabian Weik <fabian.weik@protonmail.com>
2024-09-25 20:10:52 +0200

    refactor poem headers

--- src/verification/poem_headers.rs
+++ src/verification/poem_headers.rs
@@ -1,5 +1,4 @@
-use anyhow::{anyhow, Result};
-#[cfg(feature = "poemheader")]
+use anyhow::anyhow;
 use poem::http::HeaderMap;
 
 use super::headers::MappedHeaders;
@@ -7,20 +6,23 @@ use super::headers::MappedHeaders;
 impl TryFrom<&HeaderMap> for MappedHeaders {
     type Error = anyhow::Error;
 
-    fn try_from(headers: &HeaderMap) -> std::result::Result<Self, Self::Error> {
+    fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
         let serial = extract_header(headers, "X-Marketplace-Signature-Serial")?;
         let algorithm = extract_header(headers, "X-Marketplace-Signature-Algorithm")?;
         let signature = extract_header(headers, "X-Marketplace-Signature")?;
 
         Ok(MappedHeaders {
-            signature: signature.to_string(),
-            algorithm: algorithm.to_string(),
-            serial: serial.to_string(),
+            signature,
+            algorithm,
+            serial,
         })
     }
 }
 
-fn extract_header<'a>(headers: &'a HeaderMap, key: &str) -> Result<&'a str> {
-    let result = headers.get(key).ok_or(anyhow!("Missing {} header", key))?.to_str()?;
-    Ok(result)
+fn extract_header(headers: &HeaderMap, key: &str) -> anyhow::Result<String> {
+    Ok(headers
+        .get(key)
+        .ok_or(anyhow!("Missing {} header", key))?
+        .to_str()?
+        .to_string())
 }
