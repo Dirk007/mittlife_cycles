3fe0324b3ffe407def4affc1d916b928ba57e44b
Dirk Faust <d.faust@mittwald.de>
2024-09-25 15:36:12 +0200

    Complete split

--- examples/simple/src/mock_backend/backend_state.rs
+++ examples/simple/src/mock_backend/backend_state.rs
@@ -1,35 +1,13 @@
-use anyhow::{anyhow, Result};
-use poem::http::HeaderMap;
-
-use crate::verification::{
+use anyhow::Result;
+use mittwald::verification::{
     fetcher::KeyFetcher, headers::SignatureHeaders, Ed25519Verifier, KeyCollection, MappedHeaders, MemoryCache,
     ReqwestFetcher, Verifier,
 };
-
-fn extract_heaader<'a>(headers: &'a HeaderMap, key: &str) -> Result<&'a str> {
-    let result = headers.get(key).ok_or(anyhow!("Missing {} header", key))?.to_str()?;
-    Ok(result)
-}
+use poem::http::HeaderMap;
 
 pub struct BackendState<T: Verifier> {
     key_collection: KeyCollection<MemoryCache<T::KeyType>, T::KeyType, ReqwestFetcher>,
     verifier: T,
-}
-
-impl TryFrom<&HeaderMap> for MappedHeaders {
-    type Error = anyhow::Error;
-
-    fn try_from(headers: &HeaderMap) -> std::result::Result<Self, Self::Error> {
-        let serial = extract_heaader(headers, "X-Marketplace-Signature-Serial")?;
-        let algorithm = extract_heaader(headers, "X-Marketplace-Signature-Algorithm")?;
-        let signature = extract_heaader(headers, "X-Marketplace-Signature")?;
-
-        Ok(MappedHeaders {
-            signature: signature.to_string(),
-            algorithm: algorithm.to_string(),
-            serial: serial.to_string(),
-        })
-    }
 }
 
 #[allow(unused)]
