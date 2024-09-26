1ec8c969e87908d50dcd4aee7540d52c6800b9e3
Fabian Weik <fabian.weik@protonmail.com>
2024-09-26 21:44:11 +0200

    run check, lint, etc. in pipeline

--- pipelines/main.go
+++ pipelines/main.go
@@ -13,6 +13,36 @@ const (
 
 type MittlifeCycles struct{}
 
+// BuildAndTestAll runs a complete pipeline, that verifies that the library and selected examples work correctly
+func (m *MittlifeCycles) BuildAndTestAll(
+	ctx context.Context,
+	source *dagger.Directory,
+) error {
+	_, err := m.Check(ctx, source)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.Lint(ctx, source)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.Test(ctx, source)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.ExampleSimpleCheck(ctx, source)
+	if err != nil {
+		return err
+	}
+
+	// TODO: integration test
+
+	return nil
+}
+
 // Check verifies that the library code compiles
 func (m *MittlifeCycles) Check(
 	ctx context.Context,
